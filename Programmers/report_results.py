dfd960298010afd76c4018bc928256665d318299
i-hyejin <floral_berry@naver.com>
2022-07-07 20:12:09 +0900

    🌻 REV report_results.py use index

--- Programmers/report_results.py
+++ Programmers/report_results.py
@@ -9,28 +9,32 @@ def solution(id_list, report, k):
 
     answer = []
 
-    id_count = [0 for i in range(len(id_list))]      # id_count : 신고 횟수
-    report_list = [[] for i in range(len(id_list))]  # report_list : 누가 신고했는지
-    id_mail = [0 for i in range(len(id_list))]       # id_mail : 결과 메일 횟수
+    id_count = [0 for i in range(len(id_list))]         # id_count : 신고 횟수
+    report_list = [[] for i in range(len(id_list))]     # report_list : 누가 신고했는지
+    id_mail = [0 for i in range(len(id_list))]          # id_mail : 결과 메일 횟수
 
     # report에서 신고 횟수 누적 & 신고자 기록
     for i in range(len(report)):
-        report_who = report[i].split(' ')[0]         # report_who : 신고자
-        report_id = report[i].split(' ')[1]          # report_id : 신고 대상 id
+        report_who = report[i].split(' ')[0]            # report_who : 신고자
+        report_id = report[i].split(' ')[1]             # report_id : 신고 대상 id
 
-        for j in range(len(id_list)):
-            if report_id == id_list[j]:                # 신고 대상 id와 id_list 가 같으면
-                if report_who not in report_list[j]:    # 신고자가 최초 신고자면
-                    report_list[j].append(report_who)   # 신고자 이름 기록
-                    id_count[j] += 1                    # 신고 횟수 1 누적
+        index = id_list.index(report_id)                # report_id를 id_list에서 찾기
+        if report_who not in report_list[index]:        # 신고자가 최초 신고자면
+            report_list[index].append(report_who)       # 신고자 이름 기록
+            id_count[index] += 1                        # 신고 횟수 1 누적
 
-    # k번 이상으로 정지된 아이디를 신고해서 메일 받아야 하는 사람 반환
+        # for j in range(len(id_list)):
+        #     if report_id == id_list[j]:                # 신고 대상 id와 id_list 가 같으면
+        #         if report_who not in report_list[j]:    # 신고자가 최초 신고자면
+        #             report_list[j].append(report_who)   # 신고자 이름 기록
+        #             id_count[j] += 1                    # 신고 횟수 1 누적
+
+    # k번 이상으로 신고된 아이디를 신고해서 메일 받아야 하는 사람 반환
     list_mail = []
     for i in range(len(id_list)):
-        if id_count[i] >= k:
-            list_mail = list_mail + report_list[i]
+        if id_count[i] >= k:                            # k번 이상 신고된 아이디
+            list_mail = list_mail + report_list[i]      # 리스트에 추가
 
-    # print(type(list_mail[0]))
     # 메일 횟수 count
     for i in range(len(list_mail)):
         index = id_list.index(list_mail[i])             # list_mail[i]에 해당하는 index 찾아서
