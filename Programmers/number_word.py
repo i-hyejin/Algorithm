f6668e8f8cc870922f72ce1acab203f0285ff376
iihye <floral_berry@naver.com>
2022-07-13 02:33:11 +0900

    🌻 REV number_word.py use join

--- Programmers/number_word.py
+++ Programmers/number_word.py
@@ -5,9 +5,19 @@ def solution(s):
 
     answer = []
 
+    dict_list = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']  # index - 숫자 변환 리스트
+    stack = []  # 한 글자씩 확인
 
+    for i in range(len(s)):
+        c = s[i]  # 문자열에서 한 글자 가져오기
+        answer.append(c) if c.isdigit() else stack.append(c)  # 숫자면 정답에 추가, 문자면 스택에 추가
+        stack_join = ''.join(stack)  # 스택 단어 합치기
 
-    return answer
+        if stack_join in dict_list:  # dict_list에 stack_join이 존재하면
+            answer.append(str(dict_list.index(stack_join)))  # index(숫자)로 정답에 추가
+            stack = []  # 스택 비우기
+
+    return int(''.join(answer))
 
 answer = solution(s)
 print(answer)
