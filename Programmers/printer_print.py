c9411ad7784f3cc0ed8ee5624d3eb73679517d71
Lee Hye Jin <74412423+iihye@users.noreply.github.com>
2022-11-19 17:28:24 +0900

    🌻 REV printer_print.py add urgent

--- Programmers/printer_print.py
+++ Programmers/printer_print.py
@@ -8,12 +8,15 @@ def solution(priorities, location):
 
     while True:
         temp = priorities.pop(0)                    # 대기목록에서 하나 꺼내기
+
         if urgent == temp:                          # 하나 꺼낸 요소가 가장 높은 우선순위이면
             answer += 1                             # 프린트
             if location == 0:                       # 위치가 0이면,
                 break                               # 반복문 중단
             else:                                   # 위치가 0이 아니면,
                 location -= 1                       # 하나 프린트 했으므로 1 감소
+            urgent = max(priorities)                # 가장 높은 우선순위 구하기
+
         else:                                       # 하나 꺼낸 요소가 가장 높은 우선순위가 아니면
             priorities.append(temp)                 # 대기목록의 제일 마지막에 넣기
             if location == 0:                       # 위치가 0이면,
