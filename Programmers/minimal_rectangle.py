7968291b286b67f09237bbaf7e8e96918c8ca2fc
Lee Hye Jin <74412423+iihye@users.noreply.github.com>
2022-11-29 21:26:41 +0900

    🌱 ADD minimal_rectangle.py

---
+++ Programmers/minimal_rectangle.py
@@ -0,0 +1,21 @@
+sizes = [[60, 50], [30, 70], [60, 30], [80, 40]]
+
+def solution(sizes):
+    answer = 0
+
+    w_list = []
+    h_list = []
+
+    for i in range(len(sizes)):
+        if sizes[i][0] > sizes[i][1]:
+            w_list.append(sizes[i][0])
+            h_list.append(sizes[i][1])
+        else:
+            w_list.append(sizes[i][1])
+            h_list.append(sizes[i][0])
+
+    answer = max(w_list) * max(h_list)
+
+    return answer
+
+print(solution(sizes))
