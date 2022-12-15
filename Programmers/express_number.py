10d843d39472186c813dbf9caa63f0cbc74f2866
Lee Hye Jin <74412423+iihye@users.noreply.github.com>
2022-12-15 19:31:21 +0900

    🌻 REV express_number.py

--- Programmers/express_number.py
+++ Programmers/express_number.py
@@ -1,18 +1,12 @@
 n = 15
 
 def solution(n):
-    answer = 0
+    answer = []
 
-    for i in range(1, n + 1):           # 1부터 n+1까지 반복
-        sum = 0                         # 연속된 자연수를 더한 합
-        for j in range(i, n + 1):       # i부터 n+1까지 반복
-            sum += j                    # 합에 자연수 더하기
-            if sum > n:                 # 합이 n보다 크면
-                pass                    # 나머지 반복문은 패스
-            if sum == n:                # 합이 n 이면
-                answer += 1             # 1가지 방법 찾음
-                pass                    # 나머지 반복문은 패스
+    for i in range(1, n, 2):        # 홀수이면서
+        if n % i == 0:              # n의 약수인 경우
+            answer.append(i)
 
-    return answer
+    return len(answer) + n % 2      # n이 홀수면 한 번 더 가능
 
 print(solution(n))
