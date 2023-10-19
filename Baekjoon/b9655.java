94a095b13c1eebe3313624d6299f1b4346ffd61c
Lee Hye Jin <74412423+iihye@users.noreply.github.com>
2023-10-19 16:11:34 +0900

    🌱 ADD b9655.java

---
+++ Baekjoon/b9655.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class b9655 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		if(N % 2 == 1) {
+			System.out.println("SK");
+		} else {
+			System.out.println("CY");
+		}
+	}
+
+}
